efceda74119576dadd253233505421100b3099da
Daniel <daniel.glez1993@gmail.com>
2020-09-07 23:34:12 +0200

    Configuration annotation added to Application

--- src/main/java/com/dannyxnas/demo/DemoNasApplication.java
+++ src/main/java/com/dannyxnas/demo/DemoNasApplication.java
@@ -2,8 +2,9 @@ package com.dannyxnas.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class DemoNasApplication {
 
